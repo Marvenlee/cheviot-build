96c26dd0d37dac3f77c4e8fd6fe8efad762b6c99
marven <marven10@gmail.com>
2024-06-02 17:00:52 +0100

    Updated Newlib cmakefile to build for right ARM CPU for RPi4. Allowed use of BLX instruction in signal trampoline code.

--- cmake/libc-newlib-interim.cmake
+++ cmake/libc-newlib-interim.cmake
@@ -2,16 +2,27 @@ cmake_minimum_required(VERSION 3.5)
 
 include(ExternalProject)
 
+if(DEFINED ENV{BOARD})
+  if("$ENV{BOARD}" STREQUAL "raspberrypi4")
+    set(CFLAGS_FOR_RPI  "-mcpu=cortex-a72 -mfpu=vfpv3-d16")
+  elseif("$ENV{BOARD}" STREQUAL "raspberrypi1")
+    set(CFLAGS_FOR_RPI "-mcpu=arm1176jzf-s")
+  else()
+    message( FATAL_ERROR "CFLAGS_FOR_RPI unknown value" )
+  endif()
+else()
+  message( FATAL_ERROR "CFLAGS_FOR_RPI undefined" )
+endif()
+
 ExternalProject_Add (
   newlib-interim
-##  URL               ftp://sourceware.org/pub/newlib/newlib-4.1.0.tar.gz
-#  PATCH_COMMAND     patch -s -p2 --forward --input=${CMAKE_CURRENT_SOURCE_DIR}/patches/newlib-4.1.0.patch	
   PREFIX            ${CMAKE_CURRENT_BINARY_DIR}/output
   SOURCE_DIR        ${CMAKE_CURRENT_SOURCE_DIR}/libc/newlib/	
   CONFIGURE_COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/libc/newlib/configure
 	                    --prefix=${CMAKE_CURRENT_BINARY_DIR}/build/native/
 	                    --target=arm-none-eabi --disable-newlib-supplied-syscalls
-	                    --enable-interwork --enable-multilib	                    
+	                    --enable-interwork --enable-multilib
+	                    CFLAGS_FOR_TARGET=${CFLAGS_FOR_RPI}
   BUILD_ALWAYS      ON
   DEPENDS           gcc-native-install binutils-native skeleton-root
   INSTALL_DIR       ${CMAKE_CURRENT_BINARY_DIR}/build/native/
