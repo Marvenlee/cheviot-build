feb944f9da22866485564de5396451090b102045
Marven Gilhespie <marven10@gmail.com>
2024-10-05 20:34:31 +0100

    Updated startup.cfg script to start gpio and mailbox drivers.

--- mk-kernel.img.sh
+++ mk-kernel.img.sh
@@ -39,6 +39,8 @@ cp build/host/sbin/sdcard           build/$KERNEL_IFS_DIR/sbin/
 cp build/host/sbin/extfs            build/$KERNEL_IFS_DIR/sbin/
 cp build/host/sbin/ifs              build/$KERNEL_IFS_DIR/sbin/
 cp build/host/sbin/devfs            build/$KERNEL_IFS_DIR/sbin/
+cp build/host/sbin/gpio             build/$KERNEL_IFS_DIR/sbin/
+cp build/host/sbin/mailbox          build/$KERNEL_IFS_DIR/sbin/
 cp build/host/etc/startup.cfg       build/$KERNEL_IFS_DIR/etc/
 cp build/host/lib/firmware/dt/rpi4.dtb       build/$KERNEL_IFS_DIR/lib/firmware/dt
 	
