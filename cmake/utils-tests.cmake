74275c9a66dee9b27a85c09722ebbf92c4ede513
Marven Gilhespie <marven10@gmail.com>
2024-11-03 15:55:01 +0000

    Added utils-tests cmakefile

---
+++ cmake/utils-tests.cmake
@@ -0,0 +1,19 @@
+cmake_minimum_required(VERSION 3.5)
+
+include(ExternalProject)
+
+ExternalProject_Add (
+	utils-tests
+	PREFIX            ${CMAKE_CURRENT_BINARY_DIR}/output
+	SOURCE_DIR        ${CMAKE_CURRENT_SOURCE_DIR}/utils/tests
+	CONFIGURE_COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/utils/tests/configure --host=arm-none-eabi
+	                    --prefix=${CMAKE_CURRENT_BINARY_DIR}/build/host	
+	BUILD_ALWAYS      ON
+	INSTALL_DIR       ${CMAKE_CURRENT_BINARY_DIR}/build/host
+	DEPENDS           newlib hal libtask libs
+	BUILD_COMMAND     make
+	INSTALL_COMMAND   pseudo make install
+)
+
+
+
